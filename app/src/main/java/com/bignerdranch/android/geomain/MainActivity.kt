045a94c01017200b681b81a2d92b5261935809b3
Tappok93 <Tappok939393@gmail.com>
2024-06-25 17:04:02 +0300

    QuizeApp

---
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -0,0 +1,140 @@
+package com.bignerdranch.android.geomain
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.bignerdranch.android.geomain.ui.theme.GeoQuizTheme
+
+private const val TAG = "MainActivity"
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var backButton: ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
+
+    @SuppressLint("ShowToast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.geoquiz)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        backButton = findViewById(R.id.back_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+
+//    TODO  Вешаем слушатель на кнопку "True"
+        trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
+//            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+//                .setGravity(Gravity.TOP, 0, 0) - не удаётся за использовать.....
+        }
+//    TODO  Вешаем слушатель на кнопку "Fasle"
+        falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+        }
+
+//    TODO  Вешаем слушатель на кнопку "Next" или ">"
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+//    TODO Вешаем слушатель на кнопку "Back" или "<" с проверкой на ноль индекса.
+
+        backButton.setOnClickListener {
+            if (currentIndex != 0) {
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            } else {
+                Toast.makeText(this, "This first question!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+//    TODO Добавляет возможноть изменять текст вопроса по нажатию на само поле вопроса.
+
+        questionTextView.setOnClickListener { view: View ->
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+//    TODO Функция смены вопроса.
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+//    TODO Функция проверки ответа на вопрос.
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
+    }
+
+// TODO Проверка жизненого цикла программы в Logcat.
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy() called")
+    }
+}
+
+
---
+++ app/src/main/java/com/bignerdranch/android/geomain/QuizViewModel.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.geomain
+
+import android.util.Log
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel {
+
+    init {
+        Log.d(TAG, "ViewModel instance created")
+    }
+
+//   override fun onCleared() {
+//        super.onCleared()
+//        Log.d(TAG, "ViewModel instance about to be destroyed")
+//    }
+}
